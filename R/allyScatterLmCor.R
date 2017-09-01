2301ee644129137664f260da3c16d82e1295915e
Helena Edlund <helena.edlund@astrazeneca.com>
2017-09-01 18:00:09 +0200

    snake case and updated with documentation

--- R/allyScatterLmCor.R
+++ R/allyScatterLmCor.R
@@ -1,47 +1,53 @@
-###################################################
-# allyScatterLmCor.R
-# 
-# Author: Helena Edlund
-# Created on: 2017-08-09
-# Modified on:
-# Purpose: linear correlation with coefficient
-# Dependencies: ggplot2, ggally
-###################################################
 
-allyScatterLmCor <- 
+#' @title linear regression with correlation threshold
+#' @description scatter plot with linear regression to be used with ggally
+#' @param data data frame 
+#' @param mapping aes passed to ggplot
+#' @param cor_method method for correlation estimation, Default: 'pearson'
+#' @param cor_use argument to cor, Default: 'pairwise.complete.obs'
+#' @param cor_threshold threshold for color switch, Default: 0.4
+#' @param cor_color colour for used at color switch, Default: 'red'
+#' @param ... PARAM_DESCRIPTION
+#' @return ggplot object (passed to ggally)
+#' @rdname ally_scatter_lm_cor
+#' @export 
+
+ally_scatter_lm_cor <- 
   function(data, mapping, 
-           corThreshold=0.4,
-           corMethod = "pearson", ...) {
+           cor_method = "pearson",
+           cor_use = "pearson",
+           cor_threshold=0.4,
+           cor_color, ...) {
     
-  xCol <- data[[deparse(mapping$x)]]
-  yCol <- data[[deparse(mapping$y)]]
-  
-  # Calculate correlation only if there are values different from zero
-  # (important for ETA corr plots)
-  if(all(xCol==0) | all(yCol==0)){
-    cor <- 0
-  }else{
-    cor <- cor(xCol, yCol, 
-               method=corMethod,
-               use="pairwise.complete.obs")
+    x_col <- data[[deparse(mapping$x)]]
+    y_col <- data[[deparse(mapping$y)]]
+    
+    # Calculate correlation only if there are values different from zero
+    # (important for ETA corr plots)
+    if(all(x_col==0) | all(y_col==0)){
+      correlation <- 0
+    }else{
+      correlation <- cor(x_col, y_col, 
+                         method=cor_method,
+                         use=cor_use)
+    }
+    
+    # Set to red colour if more or less than threshold
+    if(correlation <= -cor_threshold | correlation >= cor_threshold) {
+      point_color <- cor_color
+    }else{
+      point_color <- "black"
+    }
+    
+    # Plot
+    if(!all(x_col==0) & !all(y_col==0)){
+      p <- 
+        ggplot(data, mapping) + 
+        geom_point(colour=point_color) + 
+        geom_smooth(method="lm")
+    }else{
+      p <- ggplot(data, mapping) + 
+        geom_point()
+    }
+    return(p)
   }
-  
-  # Set to red colour if more or less than threshold
-  if(cor <= -corThreshold | cor >= corThreshold) {
-    pointColour <- "red" 
-  }else{
-    pointColour <- "black"
-  }
-  
-  # Plot
-  if(!all(xCol==0) & !all(yCol==0)){
-    p <- 
-      ggplot(data, mapping) + 
-      geom_point(colour=pointColour) + 
-      geom_smooth(method="lm")
-  }else{
-    p <- ggplot(data, mapping) + 
-      geom_point()
-  }
-  return(p)
-}
