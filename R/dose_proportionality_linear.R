67319922f17cb8c6474bd3018962eb1a2bd5539b
Helena Edlund <helena.edlund@astrazeneca.com>
2017-09-11 20:28:33 +0200

    updated, now working

--- R/dose_proportionality_linear.R
+++ R/dose_proportionality_linear.R
@@ -1,7 +1,34 @@
+#' @title Dose proportionality test (linear model)
+#' @description lm fit to test for dose proportionality
+#' @param df data frame
+#' @param x x variable (e.g. dose)
+#' @param y y parameter (e.g. auc)
+#' @param ci.level confidence level for lm fit in plot, Default: 0.95
+#' @param plot.par add table with lm parameters, Default: F
+#' @param x_min lower limit passed to coord_carteesian (absolute), Default: 0
+#' @param max_position upper y limit passed to coord_carteesian (relative max(y)), Default: 1.2
+#' @param min_tab_position lower y-value for parameter table (relative max(y)), Default: 1.05
+#' @param min_p_position position of p-value string (relative max(y)), Default: 0.9
+#' @param signif number of significant numbers in parameter table, Default: 3
+#' @param p_value_threshold p-value threshold for inclusion of intercept, Default: 0.05
+#' @return list with plot, lm summary and lm fit
+#' @rdname dose_proportionality_linear
+#' @export 
+#' @importFrom gridExtra tableGrob
+#' @importFrom rlang enexpr quo eval_tidy expr_text
+#' @importFrom tibble rownames_to_column
+#' @importFrom tibble rownames_to_column
+#' @import magrittr
 
 dose_proportionality_linear <- 
-  function(df, x, y, plot.par=F, max_position = 1.25,
-           min_position = 0.85, ci.level=0.95){
+  function(df, x, y, ci.level=0.95, 
+           plot.par=F, 
+           x_min = 0, 
+           max_position = 1.20,
+           min_tab_position = 1.05,
+           min_p_position = 0.90, 
+           signif=3,
+           p_value_threshold=0.05){
     
     x <- rlang::enexpr(x)
     y <- rlang::enexpr(y)
@@ -24,7 +51,7 @@ dose_proportionality_linear <-
     lm_sum <- summary(lm_fit)
     p_value_intercept <- signif(lm_sum$coeff[1,'Pr(>|t|)'], digits=3)
     
-    if(p_value_intercept > 0.05){
+    if(p_value_intercept > p_value_threshold){
       lm_fit <- rlang::eval_tidy(
         rlang::quo(
           lm(formula=UQ(y)~-1+UQ(x), data=newdf, weights=wts)
@@ -34,62 +61,94 @@ dose_proportionality_linear <-
     }
     
     # plot settings 
-    y_max <- max(df[[rlang::expr_text(y)]], na.rm=T)*max_position
-    y_min_pos <- max(df[[rlang::expr_text(y)]], na.rm=T)* min_position
+    y_max <- max(df[[rlang::expr_text(y)]], na.rm=T)
+    y_max_pos <- y_max*max_position
+    y_pval <- y_max*min_p_position
+    y_min_tab <- y_max*min_tab_position
     
     x_max <- max(df[[rlang::expr_text(x)]], na.rm=T)
-    x_min_tab <- max(df[[rlang::expr_text(x)]], na.rm=T)/4
+    x_diff <- (x_max - x_min) / 4
+    x_min_pos <- x_min + x_diff
+    x_max_pos <- x_max - x_diff
     
-    # plot without intercept
-    if(!plot.par & p_value_intercept>0.05){
-      p <- rlang::quo(
-        ggplot(newdf, aes(x=!!x, y=!!y)) + 
-          geom_point() + 
-          geom_smooth(aes(weight=wts), method="lm", formula=y~-1+x, 
-                      se=T, level=ci.level) + 
-          coord_cartesian(xlim = c(0, !!x_max), ylim = c(0, !!y_max)) + 
-          annotate(geom="text", xmin=!!x_min_tab, xmax=!!x_max,  ymin=!!y_min_pos, ymax=!!y_max, 
-                   label= paste("Weighted Linear Regression Model/n", 
-                                "p value for intercept = ", !!p_value_intercept))
-        
-  # update:  annotate with geom_text should only have x and y
-        
-      )
-    } 
-    # plot with intercept
-    if(!plot.par & p_value_intercept<=0.05){
-      p <- rlang::quo(
-        ggplot(newdf, aes(x=!!x, y=!!y)) + 
-          geom_point() + 
-          geom_smooth(aes(weight=wts), method="lm", formula=y~+x, 
-                      se=T, level=ci.level) + 
-          coord_cartesian(xlim = c(0, !!x_max), ylim = c(0, !!y_max))+ 
-          annotate(geom="text", xmin=!!x_min_tab, xmax=!!x_max,  ymin=!!y_min_pos, ymax=!!y_max, 
-                   label= paste("Weighted Linear Regression Model/n", 
-                                "p value for intercept = ", !!p_value_intercept))
-      )
+    # plots without parameters
+    if(!plot.par){
+      # 1. without intercept
+      if(p_value_intercept>p_value_threshold){
+        p <- rlang::quo(
+          ggplot(newdf, aes(x=!!x, y=!!y)) + 
+            geom_point() + 
+            geom_smooth(aes(weight=wts), method="lm", formula=y~-1+x, 
+                        se=T, level=ci.level) + 
+            coord_cartesian(xlim = c(!!x_min, !!x_max), ylim = c(0, !!y_max_pos)) + 
+            labs(title="Weighted Linear Regression Model") + 
+            annotate(geom="text", x=!!x_min_pos, y=!!y_pval, 
+                     label=paste0("p value for intercept = ", !!p_value_intercept))
+        )
+      } 
+      # 2. with intercept
+      if(p_value_intercept<=p_value_threshold){
+        p <- rlang::quo(
+          ggplot(newdf, aes(x=!!x, y=!!y)) + 
+            geom_point() + 
+            geom_smooth(aes(weight=wts), method="lm", formula=y~x, 
+                        se=T, level=ci.level) + 
+            coord_cartesian(xlim = c(!!x_min, !!x_max), ylim = c(0, !!y_max_pos)) + 
+            labs(title="Weighted Linear Regression Model")
+        )
+      }
     }
     
-    #     
-    # # else: 
-    #     text(c(bb[1],bb[1]),c(b*0.95,b*0.90),c("Intercept","Slope"),cex=cex*0.68)
-    #     for (i in 2:(length(bb))){
-    #       text(c(bb[i],bb[i]),c(b*0.95,b*0.90),c(round(summary(lm.fit)$coe[1,],3)[i-1],round(summary(lm.fit)$coe[2,],3)[i-1]),cex=cex*0.68)
-    #     }
-    #   }
-    #   
-    #   
-    #   if(plot.par){
-    #     rowname<-c("Parameter",names(summary(lm.fit)$coe[1,]))
-    #     bb<-seq(1,max(x),max(x)/5)
-    #     for (i in 1:length(bb)){text(bb[i],b,rowname[i],cex=cex*0.68)}  
-    #     abline(h=b*0.85)
-    #     n<-nrow(summary(lm.fit)$coe)
-    #     if (n==1) {
-    #       text(bb[1],b*0.90,"Slope",cex=cex*0.68)
-    #       for (i in 3:(length(bb))) {text(bb[i],b*0.90,round(summary(lm.fit)$coe,2)[i-1],cex=cex*0.68)}}
-    #   }
-    return(list(plot = rlang::eval_tidy(p), summary=summary(lm_fit)))
+    # plots with parameters
+    if(plot.par){
+      # Extract the data from the summary to a data.frame for plotting
+      # 1. lm parameter data 
+      lm_data <- as.data.frame(lm_sum$coefficients)
+      lm_data <- format(lm_data, digits=signif)
+      lm_data <- tibble::rownames_to_column(lm_data)
+      names(lm_data)[1] <- "Parameter"
+      lm_data[, !names(lm_data) %in% "Parameter"]
+      
+      # Rename parameters to intercept and slope
+      if(any(str_detect(lm_data$Parameter, "\\(Intercept\\)"))){
+        lm_data$Parameter <- 
+          str_replace(lm_data$Parameter, "\\(Intercept\\)", "Intercept")
+      }
+      lm_data$Parameter <- 
+        str_replace(lm_data$Parameter, rlang::expr_text(x), "Slope")
+      
+      # 1. without intercept
+      if(p_value_intercept>p_value_threshold){
+        p <- rlang::quo(
+          ggplot(newdf, aes(x=!!x, y=!!y)) + 
+            # add lm parameters (first layer to make sure not overplotting outliers)
+            annotation_custom(gridExtra::tableGrob(lm_data, rows=NULL),
+                              xmin=!!x_min_pos, xmax=!!x_max_pos,
+                              ymin=!!y_min_tab, ymax=!!y_max_pos) + 
+            geom_point() + 
+            geom_smooth(aes(weight=wts), method="lm", formula=y~-1+x, 
+                        se=T, level=ci.level) + 
+            coord_cartesian(xlim = c(!!x_min, !!x_max), ylim = c(0, !!y_max_pos)) + 
+            labs(title="Weighted Linear Regression Model") + 
+            annotate(geom="text", x=!!x_min_pos, y=!!y_pval, 
+                     label= paste0("p value for intercept = ", !!p_value_intercept))
+        )
+      } 
+      # 2. with intercept
+      if(p_value_intercept<=p_value_threshold){
+        p <- rlang::quo(
+          ggplot(newdf, aes(x=!!x, y=!!y)) + 
+            # add lm parameters (first layer to make sure not overplotting outliers)
+            annotation_custom(gridExtra::tableGrob(lm_data, rows=NULL),
+                              xmin=!!x_min_pos, xmax=!!x_max_pos,
+                              ymin=!!y_min_tab, ymax=!!y_max_pos) + 
+            geom_point() + 
+            geom_smooth(aes(weight=wts), method="lm", formula=y~x, 
+                        se=T, level=ci.level) + 
+            coord_cartesian(xlim = c(!!x_min, !!x_max), ylim = c(0, !!y_max_pos)) + 
+            labs(title="Weighted Linear Regression Model")
+        )
+      }
+    }
+    return(list(plot = rlang::eval_tidy(p), summary=summary(lm_fit), fit=lm_fit))
   }
-
-
