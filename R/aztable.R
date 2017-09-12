727e7d487cf1cc9760426fb11902086dc129fc6c
Helena Edlund <helena.edlund@astrazeneca.com>
2017-09-12 21:54:01 +0200

    updated with documentation and warnings

--- R/aztable.R
+++ R/aztable.R
@@ -1,23 +1,15 @@
-###################################################
-# aztable.R
-# 
-# Author: Helena Edlund
-# Created on: 2017-08-02
-# Modified on: 2017-08-03
-# Purpose: Wrapper for az specifications of pixiedust tables
-# Dependencies: github(nutterb/pixiedust)
-###################################################
 
-#' @title az specific pixidust table
-#' @description  Wrapper for az specifications of pixiedust tables
+#' @title pixiedust::dust with az specifications
+#' @description  Wrapper for pixiedust::dust with az specifications
 #' @param df data frame
 #' @param caption string with caption for table, Default: NULL
 #' @param label string with label for cross-reference. "tab:" is automatically added, Default: NULL
 #' @param print_method "latex" or "html",  Default: "latex" 
 #' @param long use longtable environment (for latex-use only), Default: F
-#' @return pixiedust object
+#' @return dust object
 #' @rdname aztable
 #' @export 
+#' @import pixiedust 
 
 aztable <- function(df, caption=NULL, label=NULL,
                     print_method="latex", long=F){
@@ -31,18 +23,23 @@ aztable <- function(df, caption=NULL, label=NULL,
     warning("Longtable environment only supported for LaTeX. Argument ignored")
   }
   
+  if(!is.null(label) & print_method!="latex") {
+    label <- NULL
+    warning("use of label only supported for LaTeX. Argument ignored")
+  }
+  
   tab <- 
-    dust(df,
+    pixiedust::dust(df,
          longtable=long,
          hhline=T, 
          caption = caption, 
          label = label) %>% 
-    sprinkle(part="head", bold=T, 
+    pixiedust::sprinkle(part="head", bold=T, 
              border=c("top", "bottom"), 
              border_thickness=3, border_units="pt") %>% 
-    sprinkle(row=nrow(df), border=c("bottom"), 
+    pixiedust::sprinkle(row=nrow(df), border=c("bottom"), 
              border_thickness=3, border_units="pt") %>% 
-    sprinkle_print_method(print_method)
+    pixiedust::sprinkle_print_method(print_method)
   
   return(tab)
 }
