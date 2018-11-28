308ff0866ac19de7b12014c3ba45db44a6e965db
Helena Edlund <helena.edlund@astrazeneca.com>
2018-11-28 14:38:33 +0000

    moved

--- developement.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-library(devtools)
-library(sinew)
-library(roxygen2)
-
-makeOxygen()
-devtools::document()
---
+++ inst/packageDevelopment/developement.R
@@ -0,0 +1,13 @@
+# script with mixed function calls to help building and documenting
+
+library(devtools)
+library(sinew)
+library(roxygen2)
+
+makeOxygen()
+devtools::document()
+
+
+# use_vignette(vignetteName, pkg="../pmxplore")
+
+# dump(function_name, file = "../pmxplore/R/file_name.R")
